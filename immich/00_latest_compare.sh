61c0b2beb1f9bc4dbd39ebb884e450e13ece760d
Wcarcamo <william.j.carcamo@gmail.com>
2024-11-07 04:36:25 +0000

    init immich

---
+++ immich/00_latest_compare.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Docker Compose
+wget -O latest-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
+diff latest-compose.yml compose.yml
+
+# Environment Variables
+wget -O latest.env https://github.com/immich-app/immich/releases/latest/download/example.env
+diff latest.env .env
+
+# Hardware Transcoding
+# wget -O hwaccel.transcoding.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.transcoding.yml
+
+# Hardware Acceleration
+# wget -O hwaccel.ml.yml https://github.com/immich-app/immich/releases/latest/download/hwaccel.ml.yml
