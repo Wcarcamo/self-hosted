6fd8fa43af9db7cc5cdc0776cfa6df87b12b264d
William Carcamo <42918151+Wcarcamo@users.noreply.github.com>
2025-01-17 22:35:14 -0800

    migrate to caddy (#5)

---
+++ compose-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ "$1" == "up" ]]; then
+  echo "Starting Containers............"
+  process="up -d"
+elif [[ "$1" == "down" ]]; then
+  echo "Stopping Containers............"
+  process=$1
+else
+  echo "Accepting only [up|down] parameters"
+  exit 1
+fi
+
+docker compose -f caddy/compose.yaml $process
+
+docker compose -f actual/compose.yaml $process
+docker compose -f calibre/compose.yaml $process
+docker compose -f dockge/compose.yaml $process
+docker compose -f filebrowser/compose.yaml $process
+docker compose -f freshrss/compose.yaml $process
+# docker compose -f gitea/compose.yaml $process
+# docker compose -f grist/compose.yaml $process
+docker compose -f homer/compose.yaml $process
+docker compose -f immich/compose.yaml $process
+docker compose -f mealie/compose.yaml $process
+docker compose -f myspeed/compose.yaml $process
+docker compose -f nginx/compose.yaml $process
+docker compose -f paperless-ngx/compose.yaml $process
