619fb1a1a99b38d199043513684050717aab1d17
William Carcamo <42918151+Wcarcamo@users.noreply.github.com>
2025-02-11 06:51:26 -0800

    update containers (#11)

--- immich/00_latest_compare.sh
+++ immich/00_latest_compare.sh
@@ -2,7 +2,7 @@
 
 # Docker Compose
 wget -O latest-compose.yml https://github.com/immich-app/immich/releases/latest/download/docker-compose.yml
-diff latest-compose.yml compose.yml
+diff latest-compose.yml compose.yaml
 
 # Environment Variables
 wget -O latest.env https://github.com/immich-app/immich/releases/latest/download/example.env
